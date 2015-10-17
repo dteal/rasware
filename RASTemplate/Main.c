3214fbb462968b96fe8b08dd5a369055c0d8c08c
dteal <daniel@teals.org>
2015-10-17 17:09:12 -0500

    Colors!

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -7,14 +7,20 @@
 // Blink the LED to show we're on
 tBoolean blink_on = true;
 tPWM * green;
+tPWM * red;
+tPWM * blue;
 float i = 0;
+float pi = 3.14159;
+
 
 void change_pwm(){
     i+=0.01;
-    if(i > 1){
+    if(i > 1000){
         i = 0;
     }
-    SetPWM(green, i, 0);
+    SetPWM(green, sin(i)*0.5+0.5, 0);
+    SetPWM(red, sin(i+pi)*0.5+0.5, 0);
+    SetPWM(blue, sin(i-pi)*0.5+0.5, 0);
 }
 
 void blink(void) {
@@ -28,10 +34,11 @@ void blink(void) {
 
 // The 'main' function is the entry point of the program
 int main(void) {
-    float pi = 3.142f;
-    
+    pi = pi * 2 / 3;
 
     green = InitializePWM(PIN_F3, 100);
+    red = InitializePWM(PIN_F1, 100);
+    blue = InitializePWM(PIN_F2, 100);
 
     CallEvery(change_pwm, 0, 0.01);
 
