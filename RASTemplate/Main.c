532fb4aa1485e72ba5cb14b00af4b0616d496fd7
dteal <daniel@teals.org>
2015-10-16 22:02:15 -0500

    Playing with PWM

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,30 +1,41 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include <RASLib/inc/pwm.h>
+#include <math.h>
 
 // Blink the LED to show we're on
 tBoolean blink_on = true;
+tPWM * green;
+int i = 0;
+
+void change_pwm(){
+    i++;
+    if(i > 100){
+        i = 0;
+    }
+    SetPWM(green, i, 10);
+}
 
 void blink(void) {
     SetPin(PIN_B0, blink_on);
     blink_on = !blink_on;
-    SetPin(PIN_F1, blink_on);
-    SetPin(PIN_F2, blink_on);
-    SetPin(PIN_F3, blink_on);
+    //SetPin(PIN_F1, blink_on);
+    //SetPin(PIN_F2, blink_on);
+    //SetPin(PIN_F3, blink_on);
 }
 
 
 // The 'main' function is the entry point of the program
 int main(void) {
-    // Initialization code can go here
-<<<<<<< HEAD
-    CallEvery(blink, 0, 0.1);
-=======
-    CallEvery(blink, 0, 0.5);
+    float pi = 3.142f;
     
->>>>>>> 38721293b469304b65d081ec8dd8ed66381b8629
+
+    green = InitializePWM(PIN_F3, 100);
+
+    CallEvery(change_pwm, 0, 0.01);
+
     while (1) {
-        // Runtime code can go here
         Printf("Hello World!\n");
     }
 }
