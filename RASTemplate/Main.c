ec250597dda612b1e591de4d09880b17d4687ed7
dteal <daniel@teals.org>
2015-11-05 22:58:56 -0600

    Add line following.

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -4,9 +4,13 @@
 #include <RASLib/inc/pwm.h>
 #include <RASLib/inc/motor.h>
 #include <RASLib/inc/adc.h>
+#include <RASLib/inc/linesensor.h>
 #include <math.h>
 
+int led = 1;
+
 /* define led objects if necessary */
+#define LIGHTS
 #ifdef LIGHTS
     tPWM * green;
     tPWM * red;
@@ -16,8 +20,12 @@
 /* define io components */
 tMotor * left_motor;
 tMotor * right_motor;
+tMotor * in_motor;
+tMotor * brush_motor;
 tADC * side_ir_sensor;
 tADC * front_ir_sensor;
+tLineSensor * line_sensor;
+tPin * line_led_pin;
 
 /* helper function to fit a value between min and max */
 float coerce(float val, float min, float max){
@@ -81,23 +89,58 @@ void adjust_motor_power(){
     }
 } /* end of adjust_motor_power function */
 
+void follow_line(){
+    float array[8];
+    LineSensorReadArray(line_sensor, array);
+    float value = array[0];
+    //Printf("%f",array[4]);
+    value *= 10;
+    value = 1-value;
+    //value *= 10;
+
+    SetPWM(red, value, 0);
+
+    if(value > 0.5){
+        SetMotor(left_motor, 0);
+        SetMotor(right_motor, -1);
+}else{
+SetMotor(left_motor, -1);
+SetMotor(right_motor, 0);}
+    //SetMotor(left_motor, value);
+    //SetMotor(right_motor, value);
+}
+
+void blink(void){
+SetPin(PIN_F1, led);
+led=!led;
+   }
+
 /* The program's main function */
 int main(void) {
 
     /* initialize motors */
     left_motor = InitializeServoMotor(PIN_E3, false);
     right_motor = InitializeServoMotor(PIN_E2, true);
+    in_motor = InitializeServoMotor(PIN_E1, true);
+    brush_motor = InitializeServoMotor(PIN_D1, true);
+    SetMotor(in_motor, 1);
+    SetMotor(brush_motor, 1);
 
     /* initialize ir sensors */
     front_ir_sensor = InitializeADC(PIN_D0);
     side_ir_sensor = InitializeADC(PIN_D3);
 
+    /* initialize line sensor */
+    line_sensor = 
+InitializeGPIOLineSensor(PIN_A3,PIN_A4,PIN_B6,PIN_B7,PIN_C6,PIN_F0,PIN_C7,PIN_B2);
+    SetPin(PIN_A2, 1);
+
     /* turn on light code if needed */
     #ifdef LIGHTS
         /* initialize rgb leds */
-        green_led = InitializePWM(PIN_F3, 500);
-        red_led = InitializePWM(PIN_F1, 500);
-        blue_led = InitializePWM(PIN_F2, 500);
+        //green = InitializePWM(PIN_F3, 500);
+        red = InitializePWM(PIN_F1, 500);
+        //blue = InitializePWM(PIN_F2, 500);
     #endif
 
     //#define MOTOR_TEST
@@ -107,9 +150,12 @@ int main(void) {
         SetMotor(right_motor, 1);
     #endif
 
+//    CallEvery(blink,0,1);
+
     /* infinite loop that program runs inside of */
     while(1){
-       adjust_motor_power();
+      follow_line();
+  //    Printf("Hi");
     }
    
 } /* end of main function */
